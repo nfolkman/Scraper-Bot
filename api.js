b9acfe1612a47ec5d0e6d47459e85a11248e37ab
nfolkman <nathanielfolkman@gmail.com>
2022-06-07 03:57:59 -0500

    fiddle around

--- api.js
+++ api.js
@@ -2,25 +2,26 @@ const express = require('express')
 const app = express()
 const scrape = require('./index.js')
 const json = require('./data.json')
+// const index = require('./index.html')
 
 
 
 const PORT = process.env.PORT || 3000
 
-let greeting= 'Daily Climate News'
+let greeting= 'To access Climate News API - add "/api" to url'
 
 
 
 // shows up on the local localhost home page
-// app.get('/', (req, res)=> {
-//    res.send(greeting) 
-//    res.send(scrape)    
-// })
+app.get('/', (req, res)=> {
+   res.send(greeting) 
+   res.send(scrape)    
+})
 
 
 // runs the index.js scraper code BUT the code doesn't show up on local server
-app.get('/', (req, res) => {
-   res.send(scrape)
+app.get('/api', (req, res) => {
+   // res.send(scrape)
    res.json(json)                    
 })
 
