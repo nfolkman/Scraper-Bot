2e28c444462c8fd135332f163904aec1e9626a68
nfolkman <nathanielfolkman@gmail.com>
2022-06-07 03:27:03 -0500

    more path edits

--- api.js
+++ api.js
@@ -12,15 +12,15 @@ let greeting= 'Daily Climate News'
 
 
 // shows up on the local localhost home page
-app.get('/', (req, res)=> {
-   res.send(greeting) 
-   res.send(scrape)    
-})
+// app.get('/', (req, res)=> {
+//    res.send(greeting) 
+//    res.send(scrape)    
+// })
 
 
 // runs the index.js scraper code BUT the code doesn't show up on local server
-app.get('/api/news', (req, res) => {
-   // res.send(scrape)
+app.get('/', (req, res) => {
+   res.send(scrape)
    res.json(json)                    
 })
 
